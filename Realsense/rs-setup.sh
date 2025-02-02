e96f797131c10a56a3f05164143633049dcbcbda
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2025-02-02 08:08:55 -0500

    Add setup script for installing Protocol Buffers with Python support

---
+++ Realsense/rs-setup.sh
@@ -0,0 +1,16 @@
+cd ~
+git clone --depth=1 -b v3.5.1 https://github.com/google/protobuf.git
+cd protobuf
+./autogen.sh
+./configure
+make -j1
+sudo make install
+cd python
+export LD_LIBRARY_PATH=../src/.libs
+python3 setup.py build --cpp_implementation 
+python3 setup.py test --cpp_implementation
+sudo python3 setup.py install --cpp_implementation
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=3
+sudo ldconfig
+protoc --version
